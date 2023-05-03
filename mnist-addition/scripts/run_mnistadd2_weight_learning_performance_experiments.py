c722d9f656602804ba93989db989f09b0c40ed80
Charles Dickens <charlieadickens@gmail.com>
2023-05-03 13:10:19 -0700

    Update run_mnistadd2_weight_learning_performance_experiments.py

--- mnist-addition/scripts/run_mnistadd2_weight_learning_performance_experiments.py
+++ mnist-addition/scripts/run_mnistadd2_weight_learning_performance_experiments.py
@@ -51,13 +51,13 @@ FIRST_ORDER_WL_METHODS_OPTION_RANGES = {
         "runtime.learn.method": ["MeanSquaredError"],
         "minimizer.objectivedifferencetolerance": ["0.1"],
         "minimizer.proxruleweight": ["1.0e-1", "1.0e-2"],
-        "minimizer.numinternaliterations": ["500", "1000"]
+        "minimizer.numinternaliterations": ["500"]
     },
     "BinaryCrossEntropy": {
         "runtime.learn.method": ["BinaryCrossEntropy"],
-        "minimizer.objectivedifferencetolerance": ["0.1"],
+        "minimizer.objectivedifferencetolerance": ["0.1", "0.01"],
         "minimizer.proxruleweight": ["1.0e-1", "1.0e-2"],
-        "minimizer.numinternaliterations": ["500", "1000"]
+        "minimizer.numinternaliterations": ["500"]
     }
 }
 
